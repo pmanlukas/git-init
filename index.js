068a19ebbe790bad48f736f5be151499551d5ce1
Lukas Pollmann <pmanlukas@github.com>
2021-02-21 12:23:07 +0000

    added intro and first error handling

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+
+const files = require('./lib/files');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('Git-Init', {horizontalLayout:'full'})
+    )
+);
+
+if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a Git respository!'));
+    process.exit();
+}
