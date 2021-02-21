3e080e8bf10308295b30fa303ee3fab8d4e10df1
Lukas Pollmann <pmanlukas@github.com>
2021-02-21 12:36:52 +0000

    add config store

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
+const Configstore = require('configstore');
+
+const conf = new Configstore('Git-Init');
 
 clear();
 
@@ -11,6 +15,13 @@ console.log(
         figlet.textSync('Git-Init', {horizontalLayout:'full'})
     )
 );
+
+const run = async () => {
+    const credentials = await inquirer.askGitHubCredentials();
+    console.log(credentials);
+};
+
+run();
 
 if (files.directoryExists('.git')) {
     console.log(chalk.red('Already a Git respository!'));
