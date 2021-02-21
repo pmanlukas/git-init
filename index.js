38f065a0bc432b8257549d13d1f2bef8e3bcf3c5
Lukas Pollmann <pmanlukas@github.com>
2021-02-21 12:52:22 +0000

    added github helper script

--- index.js
+++ index.js
@@ -3,25 +3,14 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
-const inquirer = require('./lib/inquirer');
-const Configstore = require('configstore');
-
-const conf = new Configstore('Git-Init');
 
 clear();
 
 console.log(
     chalk.yellow(
-        figlet.textSync('Git-Init', {horizontalLayout:'full'})
+        figlet.textSync('git-init', {horizontalLayout:'full'})
     )
 );
-
-const run = async () => {
-    const credentials = await inquirer.askGitHubCredentials();
-    console.log(credentials);
-};
-
-run();
 
 if (files.directoryExists('.git')) {
     console.log(chalk.red('Already a Git respository!'));
